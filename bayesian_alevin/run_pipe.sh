6076684046d53b4ca624585fb709b321238361f4
Avi Srivastava <k3yavi@users.noreply.github.com>
2020-02-04 22:45:01 +0530

    Update run_pipe.sh

--- bayesian_alevin/run_pipe.sh
+++ bayesian_alevin/run_pipe.sh
@@ -1,25 +1,25 @@
-## alevin em
-# /mnt/scratch5/laraib/alevin2/salmon/bin/salmon alevin -lISR -1 ./4k_simulation/hg_100_S1_L001_R1_001.fastq.gz -2 ./4k_simulation/hg_100_S1_L001_R2_001.fastq.gz -o ./em -i /mnt/scratch7/avi/snare-seq/indices/human_index --tgMap /mnt/scratch7/avi/snare-seq/reference/human/txp2gene.txt --chromium --whitelist ./4k_simulation/minnow_whitelist.txt
+# alevin em
+/mnt/scratch5/laraib/alevin2/salmon/bin/salmon alevin -lISR -1 ./4k_simulation/hg_100_S1_L001_R1_001.fastq.gz -2 ./4k_simulation/hg_100_S1_L001_R2_001.fastq.gz -o ./em -i /mnt/scratch7/avi/snare-seq/indices/human_index --tgMap /mnt/scratch7/avi/snare-seq/reference/human/txp2gene.txt --chromium --whitelist ./4k_simulation/minnow_whitelist.txt
 
-## seurat mapping
-# for i in {1..60}
-# do
-# 	Rscript --vanilla run_seurat.R
-# 	mkdir priorNum"$i"
-# 	mv ./*.txt priorNum"$i"
-# 	cd priorNum"$i"
+# seurat mapping
+for i in {1..30}
+do
+	Rscript --vanilla run_seurat.R
+	mkdir priorNum"$i"
+	mv ./*.txt priorNum"$i"
+	cd priorNum"$i"
 
-# 	paste <(cut -f1 -d'_' refs.txt) <(cut -f1 -d'_' query.txt) > mappings.txt
-# 	awk '($1 != $2)' mappings.txt > mappings_uniq.txt
-# 	awk '($1 != $2)' mappings.txt | cut -f1 | sort | uniq > whitelist.txt
-# 	cd ../
+	paste <(cut -f1 -d'_' refs.txt) <(cut -f1 -d'_' query.txt) > mappings.txt
+	awk '($1 != $2)' mappings.txt > mappings_uniq.txt
+	awk '($1 != $2)' mappings.txt | cut -f1 | sort | uniq > whitelist.txt
+	cd ../
 
-# done
+done
 
-## instead of average, use all mappings together
-# cat priorNum{1..30}/mappings_uniq.txt | sort | uniq > mappings.txt
-# mkdir prior
-# python generate_prior.py
+# instead of average, use all mappings together
+cat priorNum{1..30}/mappings_uniq.txt | sort | uniq > mappings.txt
+mkdir prior
+python generate_prior.py
 
 ## alevin vbem
 /mnt/scratch5/laraib/alevin2/salmon/bin/salmon alevin -lISR -1 ./4k_simulation/hg_100_S1_L001_R1_001.fastq.gz -2 ./4k_simulation/hg_100_S1_L001_R2_001.fastq.gz -o ./vbem -i /mnt/scratch7/avi/snare-seq/indices/human_index --tgMap /mnt/scratch7/avi/snare-seq/reference/human/txp2gene.txt --chromium --whitelist ./4k_simulation/minnow_whitelist.txt --vbemPrior prior --vbemNorm 20055723
